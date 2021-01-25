a4a0bf703cb667cb2360a84f3b0e7e3cdeb5ab88
张冬 <dong.zhang@yingxiong.com>
2021-01-25 10:26:35 +0800

    修改 修改无重复字符串最长子串算法

--- src/test/java/com/example/springbootdemo/arithmetic/LengthOfLongestSubstring.java
+++ src/test/java/com/example/springbootdemo/arithmetic/LengthOfLongestSubstring.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class LengthOfLongestSubstring {
 
     public static void main(String[] args) {
-        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring2("aabaab!bb"));
+        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("pwwkew"));
     }
 
     /**
@@ -19,20 +19,20 @@ public class LengthOfLongestSubstring {
      *
      * @return int
      * @params [s]
-     * @author dong.zhang
+     * @author jingjing.meng
      * @date 2021/1/13 19:28
      **/
-    public int lengthOfLongestSubstring(String s) {
-        int len = 0, max = 0;
+    public int lengthOfLongestSubstring(String str) {
+        int index = 0, max = 0;
         Set<Character> result = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            while (len < s.length() && !result.contains(s.charAt(len))) {
-                result.add(s.charAt(len));
-                ++len;
+        for (int i = 0; i < str.length(); i++) {
+            while (index < str.length() && !result.contains(str.charAt(index))) {
+                result.add(str.charAt(index));
+                index++;
             }
             System.out.println(result.toString());
-            max = Math.max(max, len - i);
-            result.remove(s.charAt(i));
+            max = Math.max(max, index - i);
+            result.remove(str.charAt(i));
         }
         return max;
     }
@@ -68,5 +68,23 @@ public class LengthOfLongestSubstring {
         }
         return 0;
     }
+
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length();
+        int res = 0;//记录最长子串的长度
+        int end = 0, start = 0;//记录开始和结尾的下标
+        Set<Character> set = new HashSet<>();//使用set容器不重复
+        while (start < n && end < n) {
+            if (set.contains(s.charAt(end))) {//如果窗口右侧的字符已经存在
+                set.remove(s.charAt(start++));//左侧窗口边界向右
+            } else {
+                set.add(s.charAt(end++));//如果窗口中无重复，窗口右侧向右滑动
+                res = Math.max(res, end - start);//同时记录当前最大长度
+            }
+        }
+        return res;
+    }
+
+
 }
 
